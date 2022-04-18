e3f3dfb3ee8f8e56d99af062c64254e48dc0afce
Akhilchary <akhilchary5418@gmail.com>
2022-04-18 15:31:16 +0530

    routes

--- edam/src/App.js
+++ edam/src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import {HashRouter as Router,Routes,Route} from "react-router-dom";
-import Home from "./Home";
-import Test from './test';
+import Homepage from  "./pages/Homepage/Homepage";
+import Contactus from './pages/Contactus/Contactus';  
+import Gallery  from './pages/Gallery/Gallery';
+import Guilds from './pages/Guilds/Guilds';
+import Joinus from './pages/Joinus/Joinus';
+import Team from './pages/Team/Team';
+import Work from './pages/Work/Work';
 function App() {
   return (
       // <Home />
     <Router>
       <Routes>
-        <Route exact path='/' element={<Test />} ></Route>
-        <Route exact path='/home' element={<Home />} ></Route>
-        {/* <Route exact path='/test' component={test} ></Route> */}
+        <Route exact path='/' element={<Homepage />} ></Route>
+        <Route path='/aboutus' element={<Contactus />} ></Route>
+        <Route path='/contactus' element={<Gallery />} ></Route>
+        <Route path='/gallery' element={<Guilds />} ></Route>
+        <Route path='/guilds' element={<Joinus />} ></Route>
+        <Route path='/joinus' element={<Team />} ></Route>
+        <Route path='/team' element={<Work />} ></Route>
+
           
       </Routes>
 
---
+++ edam/src/pages/Guilds/Guilds.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Guilds = () => {
+  return (
+    <div>Guilds</div>
+  )
+}
+
+export default Guilds
